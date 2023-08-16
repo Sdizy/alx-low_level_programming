c7ffeed6a3ccc2c4d5742b040ead6bb1445dfa20
Sdizy <saohatsesamkelo@gmail.com>
2023-08-16 07:53:04 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - A program that prints _putchar
@@ -9,14 +8,15 @@
 int main(void)
 {
 
-	_putchar('_');
-       	_putchar('p');
-	_putchar('u');
-       	_putchar('t');
-       	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
