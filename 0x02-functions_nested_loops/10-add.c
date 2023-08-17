01537c54230adc5cc9817497b64e967b8314fe15
Sdizy <saohatsesamkelo@gmail.com>
2023-08-17 05:56:04 -0700

    git add . files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - A function that adds 2 ingatagers and returns the results
+ * @a: int to be added in b
+ * @b: int to be added in a
+ * Return: Sum of a nd b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
